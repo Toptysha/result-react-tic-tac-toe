d1cfc2b3057f8112372cb8cf6d04a611ea13c286
Vladislav Baranov <toptun113@gmail.com>
2023-12-15 16:02:33 +0300

    array transfer

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import { useState } from 'react';
 import styles from './index.module.css';
 
+const allButtons = [ 
+  ['7', '8', '9'], 
+  ['4', '5', '6'], 
+  ['1', '2', '3'], 
+  ['clear', '0', '←'],
+  ['+', '-', '=']
+]
+
+const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
 function App() {
   const [operand1, setOperand1] = useState('0')
   const [operand2, setOperand2] = useState('')
   const [operator, setOperator] = useState('')
-
-  const allButtons = [ 
-    ['7', '8', '9'], 
-    ['4', '5', '6'], 
-    ['1', '2', '3'], 
-    ['clear', '0', '←'],
-    ['+', '-', '=']
-  ]
-
-  const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
   const calcButtons = allButtons.map(e => {
     return (
